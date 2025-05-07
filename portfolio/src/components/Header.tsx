589670e14446677042bf034e2f0cc786d8827a57
Michael Chen <michaelchen@BoopBoop.local>
2025-05-07 00:24:16 -0700

    finishing button

--- portfolio/src/components/Header.tsx
+++ portfolio/src/components/Header.tsx
@@ -20,17 +20,17 @@ export default function Header() {
                     </a>
                 </div>
                 <nav className={`${outfit.className} flex w-125 justify-around font-normal animate__bounceIn animate__animated`}>
-                    <a href="#about" className={`text-2xl scroll-smooth rounded-xl p-2 border-2 border-transparent hover:border-black hover:text-3xl transition-text transition-border duration-150`}>
+                    <a href="#about" className={`text-2xl scroll-smooth rounded-xl p-2 border-3 border-transparent hover:border-black hover:text-3xl transition-text transition-border duration-150`}>
                     About Me
                     </a>
-                    <a href="/projects" className={`text-2xl scroll-smooth rounded-xl p-2 border-2 border-transparent hover:border-black hover:text-3xl transition-text transition-border duration-150`}>
+                    <a href="/projects" className={`text-2xl scroll-smooth rounded-xl p-2 border-3 border-transparent hover:border-black hover:text-3xl transition-text transition-border duration-150`}>
                     Projects
                     </a>
-                    <a href="/contact" className={`text-2xl scroll-smooth rounded-xl p-2 border-2 border-transparent hover:border-black hover:text-3xl transition-text transition-border duration-150`}>
+                    <a href="/contact" className={`text-2xl scroll-smooth rounded-xl p-2 border-3 border-transparent hover:border-black hover:text-3xl transition-text transition-border duration-150`}>
                     Contact
                     </a>
                 </nav>
-                <button className={`${outfit.className} text-2xl border-solid animate__bounceIn animate__animated`}>Contact Me</button>
+                <button className={`${outfit.className} text-2xl border-3 border-black p-2 rounded-xl animate__bounceIn animate__animated hover:p-3 transition-p duration-150`}>Contact Me</button>
         
             </div>
         </header>
