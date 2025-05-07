0cc98edaafe55984916be938a29bbb9dacc541d7
Michael Chen <michaelchen@BoopBoop.local>
2025-05-06 17:43:58 -0700

    adding hover state for nav

--- portfolio/src/components/Header.tsx
+++ portfolio/src/components/Header.tsx
@@ -5,7 +5,7 @@ import 'animate.css'
 export default function Header() {
 
     return (
-        <header className="sticky fixed z-50 w-full h-full bg-neutral-50">
+        <header className="sticky fixed top-0 z-50 w-full h-25 bg-neutral-50">
             <div className="flex items-center justify-between w-full h-full px-10">
                 <div className="flex items-center p-3">
                     <a href="#" className="text-2xl font-bold text-foreground animate__bounceIn animate__animated">
@@ -19,13 +19,13 @@ export default function Header() {
                     </a>
                 </div>
                 <nav className={`${outfit.className} flex w-125 justify-around font-normal animate__bounceIn animate__animated`}>
-                    <a href="#about" className={`text-2xl scroll-smooth hover:underline`}>
+                    <a href="#about" className={`text-2xl scroll-smooth rounded-xl p-2 border-2 border-transparent hover:border-black hover:text-3xl transition-text transition-border duration-300`}>
                     About Me
                     </a>
-                    <a href="/projects" className={`text-2xl hover:underline`}>
+                    <a href="/projects" className={`text-2xl scroll-smooth rounded-xl p-2 border-2 border-transparent hover:border-black hover:text-3xl transition-text transition-border duration-300`}>
                     Projects
                     </a>
-                    <a href="/contact" className={`text-2xl hover:underline`}>
+                    <a href="/contact" className={`text-2xl scroll-smooth rounded-xl p-2 border-2 border-transparent hover:border-black hover:text-3xl transition-text transition-border duration-300`}>
                     Contact
                     </a>
                 </nav>
