6f58a8267f13d62338c66d913837739e80dc0ffc
Michael Chen <chenmichael2012@gmail.com>
2025-05-10 00:57:00 -0700

    reorganizing to make the header and page look better

--- portfolio/src/components/Header.tsx
+++ portfolio/src/components/Header.tsx
@@ -18,7 +18,7 @@ export default function Header() {
     ]
 
     return (
-        <header className="sticky z-50 w-full h-25 backdrop-blur-sm">
+        <header className="sticky top-0 z-50 w-full h-20 backdrop-blur-xl">
             <div className="flex items-center justify-between w-full h-full px-10">
                 <div className="">
                     <a href="#" className="text-2xl animate__bounceIn animate__animated">
@@ -34,7 +34,7 @@ export default function Header() {
                 </div>
                 <nav className={`${poppins.className} ml-20 font-light animate__bounceIn animate__animated`}>
                     {nav_list.map((item) => (
-                        <a key={item.name} href={item.href} className={`text-2xl scroll-smooth rounded-xl p-2 border-3 border-transparent hover:border-black hover:text-3xl hover:font-normal transition-text transition-border duration-150`}>{item.name}</a>
+                        <a key={item.name} href={item.href} className={`text-xl scroll-smooth rounded-xl p-2 mx-3 border-3 border-transparent hover:border-black hover:text-3xl hover:font-normal transition-text transition-border duration-150`}>{item.name}</a>
                     ))}
                 </nav>
                 <div className="flex">
--- portfolio/src/components/Hero.tsx
+++ portfolio/src/components/Hero.tsx
@@ -12,26 +12,29 @@ export default function Hero() {
   return (
     <section className="flex flex-col justify-evenly w-full h-[calc(100vh-100px)] bg-gray-50">
       <div className={`${poppins.className} flex items-center justify-around leading-none w-full`}>
-        <div className="profile-picture-container inline-block relative overflow-hidden w-110 h-110 rounded-full">
-          <img className="profile-picture w-auto h-full transform scale-400 mx-8 my-110" src="/images/profile1.jpeg" alt="Michael Chen Picture"/>
-        </div>
         <div className="flex flex-col">
-          <h1 className="font-extrabold pr-25 text-[100px] text-primary">Michael</h1>
-          <h1 className="self-end font-extrabold text-[100px] text-secondary">Chen</h1>
-          <p className="relative bottom-17">
-            <span className="flex items-center text-2xl">
-            <Image 
-              className="rotate-340"
-              src="/pin.svg"
-              width={40}
-              height={40}
-              alt="Pin"
-            />Torrance, CA
+          <h1 className="font-extrabold pr-25 text-[8vw] text-primary">Michael</h1>
+          <div className="flex items-end">
+            <h1 className="font-extrabold text-[8vw] text-secondary">Chen</h1>
+            <span className="flex flex-row items-center mb-4 text-black text-[1.5vw] font-medium">
+              <Image 
+                className="rotate-340"
+                src="/pin.svg"
+                width={40}
+                height={40}
+                alt="Pin"
+              />
+              <p>Torrance, CA</p>
             </span>
+          </div>
+          <div className="">
             <p className="text-3xl mt-9">Software Engineer</p>
-          </p>
-          
+          </div>
         </div>
+        <div className="profile-picture-container inline-block relative overflow-hidden w-110 h-110 rounded-full md:w-80 md:h-80 hover:scale-100">
+          <img className="profile-picture w-auto h-full transform scale-400 mx-8 my-110 md:scale-300 md:mx-7 md:my-60" src="/images/profile1.jpeg" alt="Michael Chen Picture"/>
+        </div>
+
       </div>
       <div>socials</div>
     </section>
