785643852307e71bff33252a914ad4f235a90b86
Michael Chen <chenmichael2012@gmail.com>
2025-05-12 20:15:36 -0700

    fixing github issue

--- portfolio/src/components/Header.tsx
+++ portfolio/src/components/Header.tsx
@@ -15,8 +15,8 @@ export default function Header() {
 
     var icon_list = [
         { name: "github", href: "https://github.com/chenmichael2", color: "#555", import: GithubIcon},
-        { name: "linkedin", href: "https://www.linkedin.com/in/chenmichael2/", color:"#0a66c2", import: LinkedinIcon},
-        { name: "email", href: "/", color:"#e7a33e", import: EmailIcon},
+        { name: "linkedin", href: "https://www.linkedin.com/in/chenmichael2/", color:"#1464c0", import: LinkedinIcon},
+        { name: "email", href: "/", color:"#fccf55", import: EmailIcon},
         { name: "resume", href: "/", color:"#6c3baa", import: ResumeIcon},
     ];
     
