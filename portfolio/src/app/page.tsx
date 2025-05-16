73b356e5590369cf43f9fce60026b7d338b044be
Michael Chen <chenmichael2012@gmail.com>
2025-05-16 14:54:54 -0700

    commenting out my scroll effect, trying to make it sticky maybe?

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -11,19 +11,19 @@ gsap.registerPlugin(ScrollTrigger);
 
 export default function Home() {
     
-  useEffect(() => {
-      const tl = gsap.timeline({
-        scrollTrigger: {
-          trigger: ".picture",
-          start: "0% 22%",
-          end: "bottom 50%",
-          markers: true,
-          scrub: true,
-        },
-    });
+  // useEffect(() => {
+  //     const tl = gsap.timeline({
+  //       scrollTrigger: {
+  //         trigger: ".picture",
+  //         start: "top 22%",
+  //         end: "+=700",
+  //         markers: true,
+  //         scrub: 1,
+  //       },
+  //   });
   
-    tl.to(".picture", { y: 700, duration: 1 });
-  }, []);
+  //   tl.to(".picture", { y: 700, duration: 1 });
+  // }, []);
   return (
     <div>
       <Hero />
