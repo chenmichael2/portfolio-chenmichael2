1919d3fc28c716bd619cf93376c08ebe40fc1ef2
Michael Chen <chenmichael2012@gmail.com>
2025-05-18 05:13:52 -0700

    fixing hover on header icons

--- portfolio/public/icons/email.tsx
+++ portfolio/public/icons/email.tsx
@@ -12,7 +12,7 @@ export default function EmailIcon({ icon }: IconProps) {
         onMouseEnter={() => setHover(true)}
         onMouseLeave={() => setHover(false)} 
         target="_blank" 
-        className={`flex items-center justify-center w-[40px] h-[40px] p-2 m-2 border border-transparent rounded-full hover:border-black hover:border-1 hover:scale-110 transition-all duration-300`}
+        className={`flex items-center justify-center w-[40px] h-[40px] p-2 m-2 border border-transparent rounded-full hover:border-black hover:border-1 hover:scale-120 transition-all duration-300`}
         >
             <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M16.6667 3.33331H3.33335C2.41288 3.33331 1.66669 4.07951 1.66669 4.99998V15C1.66669 15.9205 2.41288 16.6666 3.33335 16.6666H16.6667C17.5872 16.6666 18.3334 15.9205 18.3334 15V4.99998C18.3334 4.07951 17.5872 3.33331 16.6667 3.33331Z" fill={`${iconColor}`} stroke="black" strokeWidth="1.66667" strokeLinecap="round" strokeLinejoin="round"/>
