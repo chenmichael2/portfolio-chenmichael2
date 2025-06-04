ea3140d4db4a948e3456ebe28656fdc35c5d5899
Michael Chen <chenmichael2012@gmail.com>
2025-06-04 00:31:34 -0700

    cleaning comment out

--- portfolio/src/components/Header.tsx
+++ portfolio/src/components/Header.tsx
@@ -92,33 +92,5 @@ export default function Header() {
                 </ul>
             </nav>
         </div>
-        // <header className="sticky top-0 z-100 w-full h-20 backdrop-blur-xl">
-        //     <div className="flex items-center justify-between w-full h-full px-10">
-        //         <div className="">
-        //             <a href="#" className="text-2xl animate__bounceIn animate__animated">
-        //                 <Image 
-        //                     className="w-25 h-25 hover:scale-120 transition-all duration-300"
-        //                     src="/logo.svg"
-        //                     alt="Logo"
-        //                     width={10}
-        //                     height={10}
-        //                     priority
-        //                 />
-        //             </a>
-        //         </div>
-        //         <nav className={`${poppins.className} ml-20 font-light animate__bounceIn animate__animated`}>
-        //             {nav_list.map((item) => (
-        //                 <a key={item.name} href={item.href} className={`text-xl scroll-smooth rounded-xl p-2 mx-3 border-3 border-transparent hover:border-black hover:text-3xl hover:font-normal transition-text transition-border duration-150`}>{item.name}</a>
-        //             ))}
-        //         </nav>
-        //         <div className="flex">
-        //             {icon_list.map((icon) => (
-        //                 <div key={icon.name}>
-        //                     {icon.import({ icon })}
-        //                 </div>
-        //             ))}
-        //         </div>
-        //     </div>
-        // </header>
-    );
+        );
     }
--- portfolio/src/components/Hero.tsx
+++ portfolio/src/components/Hero.tsx
@@ -31,38 +31,6 @@ export default function Hero() {
         height={300}
         className="rounded-xl"
       />
-
-      {/* <div className={`${poppins.className} flex items-center justify-evenly leading-none w-full`}>
-        <div className="flex flex-col">
-          <h1 className="font-extrabold pr-25 text-[8vw] text-primary animate__animated animate__slideInDown">Michael</h1>
-          <div className="flex items-end">
-            <h1 className="font-extrabold text-[8vw] text-secondary animate__animated animate__slideInDown">Chen</h1>
-          </div>
-          <div>
-            <div className="flex flex-row items-center mb-4 text-black text-[1.5vw] font-medium animate__animated animate__slideInDown">
-              <Image 
-                className="rotate-340"
-                src="icons/pin.svg"
-                width={40}
-                height={40}
-                alt="Pin"
-              />
-              <p>Torrance, CA</p>
-            </div>
-            <div>
-              {tagData.map((tag) => (
-                <div key={tag.title} className={`inline-block px-4 py-2 mr-2 text-sm font-medium text-gray-700 rounded-full animate__animated animate__slideInDown`} style={{ backgroundColor: tag.color }}>
-                  {tag.title}
-                </div>
-              ))}
-            </div>
-          </div>
-        </div>
-        <div className="relative inline-block overflow-hidden w-100 h-100 border-1 border-neutral-300 shadow-2xl rounded-full hover:scale-105 transition-all duration-300 ease-in-out">
-          <Image width={100000} height={100000} className="w-auto h-full origin-center transition-all duration-300 ease-in-out" src="/images/profile.jpeg" alt="Michael Chen Picture"/>
-        </div>
-      </div>
-      <div>socials</div> */}
     </section>
   );
 }
