770ed16475e0a08d5126e5a5ac3998e5b20e891c
Michael Chen <chenmichael2012@gmail.com>
2025-06-05 00:35:00 -0700

    adding the blob

--- portfolio/src/components/Hero.tsx
+++ portfolio/src/components/Hero.tsx
@@ -24,6 +24,7 @@ export default function Hero() {
       <div className={`${openSauce.className} text-5xl font-medium flex flex-col w-full p-6`}>
         <span className="self-start">Michael</span><span className="self-end">Chen</span>
       </div>
+      <div className="w-100 h-100 bg-blue-500 overflow-hidden" style={{borderRadius: '73% 27% 78% 22% / 86% 63% 37% 14% '}}></div>
       <Image
         src="/images/full-profile-no-bg.png"
         alt="Hero Image"
