622f08f9e3fd1c3ac2cd7952006e969c668424c1
Michael Chen <chenmichael2012@gmail.com>
2025-06-09 23:21:31 -0700

    moving classes to make it easier

--- portfolio/src/components/Hero.tsx
+++ portfolio/src/components/Hero.tsx
@@ -21,8 +21,8 @@ export default function Hero() {
   ]
 
   return (
-    <section className="pt-15 w-full h-[calc(100vh-2rem)] bg-gray-50">
-      <div className="relative overflow-x-hidden w-full h-full">
+    <section className="pt-14 w-full h-[calc(100vh-1rem)] bg-gray-50">
+      <div className="relative overflow-hidden w-full h-full">
         <svg className="absolute w-100 h-100 overflow-hidden left-1/2 -translate-x-1/2 -translate-y-7 scale-130" viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
           <path fill="#2E72AC" d="M41.3,-51.1C55.2,-37.6,69.3,-26.1,74.9,-10.8C80.5,4.6,77.6,23.9,69.2,41.5C60.8,59,47,74.9,29.9,80.4C12.8,85.9,-7.4,80.9,-25.4,73C-43.4,65,-59.1,54.1,-68.6,39C-78.1,23.8,-81.3,4.5,-79.3,-15.2C-77.2,-34.8,-69.9,-54.7,-55.8,-68.2C-41.7,-81.7,-20.9,-88.8,-3.6,-84.6C13.7,-80.3,27.4,-64.7,41.3,-51.1Z" transform="translate(100 100)" />
         </svg>
@@ -39,9 +39,10 @@ export default function Hero() {
           height={600}
           className="absolute rounded-xl z-10 left-1/2 -translate-x-1/2 -translate-y-17"
         />
-        <div className={`${openSauce.className} relative z-12 text-secondary text-6xl font-bold flex flex-col w-full p-6 text-center translate-y-60 mt-10`}>
-          <span className="flex justify-center align-center">Michael Chen</span>
-        </div>
+        <div className={`${openSauce.className} relative z-12 flex flex-col w-full p-6 text-center translate-y-60 mt-10`}>
+          <span className="text-secondary text-6xl font-bold">Michael Chen</span>
+          <div className="relative z-50">software engineer</div>
+        </div> 
       </div>
     </section>
   );
