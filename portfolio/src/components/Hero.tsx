191a11643cb9c844ddc01e9f97029e0c5ab36d94
Michael Chen <chenmichael2012@gmail.com>
2025-06-09 18:46:30 -0700

    new blobs

--- portfolio/src/components/Hero.tsx
+++ portfolio/src/components/Hero.tsx
@@ -27,18 +27,18 @@ export default function Hero() {
   return (
     <section className="pt-15 w-full h-[calc(100vh-2rem)] bg-gray-50">
       <div className="relative overflow-x-hidden w-full h-full">
-        <div className={`${openSauce.className} absolute z-12 text-secondary text-5xl font-medium flex flex-col w-full p-6 text-center translate-y-76`}>
+        <div className={`${openSauce.className} absolute z-12 text-secondary text-5xl font-medium flex flex-col w-full p-6 text-center translate-y-60`}>
           <span className="flex justify-center align-center">Michael Chen</span>
         </div>
         {/* <div className="absolute w-100 h-100 bg-primary overflow-hidden left-1/2 -translate-x-1/2" style={{borderRadius: '73% 27% 78% 22% / 86% 63% 37% 14% '}}></div> */}
         <svg className="absolute w-100 h-100 overflow-hidden left-1/2 -translate-x-1/2 -translate-y-7" viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
           <path fill="#2E72AC" d="M41.3,-51.1C55.2,-37.6,69.3,-26.1,74.9,-10.8C80.5,4.6,77.6,23.9,69.2,41.5C60.8,59,47,74.9,29.9,80.4C12.8,85.9,-7.4,80.9,-25.4,73C-43.4,65,-59.1,54.1,-68.6,39C-78.1,23.8,-81.3,4.5,-79.3,-15.2C-77.2,-34.8,-69.9,-54.7,-55.8,-68.2C-41.7,-81.7,-20.9,-88.8,-3.6,-84.6C13.7,-80.3,27.4,-64.7,41.3,-51.1Z" transform="translate(100 100)" />
         </svg>
-        <svg className="absolute w-100 h-100 overflow-hidden left-1/2 -translate-x-42 rotate-90 translate-y-20" viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
-          <path fill="#235784" d="M36.1,-54.1C48.8,-55.1,62.5,-49.6,64.1,-39.4C65.7,-29.3,55.3,-14.7,49.2,-3.5C43.2,7.7,41.6,15.4,41,27.3C40.4,39.3,40.9,55.5,34.2,66.4C27.6,77.4,13.8,83.1,-0.3,83.7C-14.4,84.3,-28.9,79.7,-40.2,71.4C-51.4,63.1,-59.6,51.1,-61.7,38.6C-63.8,26,-59.9,13,-56.6,1.9C-53.4,-9.3,-50.7,-18.5,-47.6,-29.2C-44.4,-39.8,-40.7,-51.9,-32.6,-53.6C-24.6,-55.2,-12.3,-46.5,-0.3,-46C11.7,-45.4,23.4,-53.1,36.1,-54.1Z" transform="translate(100 100)" />
+        <svg className="absolute w-100 h-100 overflow-hidden left-1/2 -translate-x-42 translate-y-0" viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
+          <path fill="#235784" d="M29.8,-49C35.4,-42.9,34.3,-29,42.6,-17.5C50.9,-6,68.7,3.2,74.2,15.3C79.7,27.4,73,42.4,60.1,45.6C47.2,48.7,28.2,40,13.3,43.6C-1.6,47.2,-12.4,63.1,-21.3,64.2C-30.1,65.4,-37,51.7,-49.7,40.9C-62.3,30.1,-80.6,22.2,-83,11.4C-85.3,0.6,-71.8,-13,-61.2,-24.5C-50.6,-36.1,-43,-45.6,-33.3,-49.8C-23.6,-54,-11.8,-52.8,0.2,-53.1C12.2,-53.3,24.3,-55,29.8,-49Z" transform="translate(100 100)" />
         </svg>
-        <svg className="absolute z-11 w-100 h-100 overflow-hidden left-1/2 -translate-x-1/2 translate-y-40" viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
-          <path fill="#BCDBDF" d="M32,-33.1C47.3,-33,69.5,-31.4,76,-22.2C82.5,-12.9,73.3,3.9,66.1,20C58.8,36,53.5,51.2,42.8,54.2C32.2,57.2,16.1,48.1,1.3,46.4C-13.5,44.6,-27.1,50.3,-36,46.6C-44.8,43,-49.1,30.2,-57.2,16C-65.3,1.9,-77.3,-13.6,-72.9,-22.6C-68.5,-31.6,-47.6,-34.3,-32.8,-34.5C-17.9,-34.7,-8.9,-32.6,-0.3,-32.2C8.4,-31.8,16.7,-33.2,32,-33.1Z" transform="translate(100 100)" />
+        <svg className="absolute z-11 w-100 h-100 overflow-hidden left-1/2 -translate-x-1/2 translate-y-30 scale-80" viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
+          <path fill="#BCDBDF" d="M38.4,-52.6C53.5,-50.1,72.1,-45.8,80.1,-34.7C88.2,-23.6,85.7,-5.8,81.5,10.6C77.4,27,71.6,42,61.9,54.2C52.2,66.3,38.6,75.6,25.2,74.3C11.8,73.1,-1.4,61.3,-13.2,53.8C-25,46.3,-35.5,43.1,-49.2,37.1C-62.9,31.2,-79.9,22.6,-83.4,10.8C-86.8,-0.9,-76.8,-15.7,-67,-27.9C-57.2,-40.1,-47.7,-49.7,-36.6,-54.1C-25.4,-58.4,-12.7,-57.4,-0.5,-56.6C11.7,-55.8,23.3,-55.1,38.4,-52.6Z" transform="translate(100 100)" />
         </svg>
         <Image
           src="/images/full-profile-no-bg.png"
