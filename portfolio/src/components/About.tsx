d8af070fa73260f88cd022708d784c739a2e84d5
Michael Chen <chenmichael2012@gmail.com>
2025-08-03 16:46:45 -0700

    removing unnecessary files and chanigng about

--- portfolio/src/components/About.tsx
+++ portfolio/src/components/About.tsx
@@ -1,14 +1,15 @@
 export default function About() {
   return (
-    <section id="about" className="flex flex-col items-center justify-center w-full bg-gray-100 animate__animated animate__fadeIn
+    <section id="about" className="flex flex-col items-center justify-center w-full bg-gray-50 border-t-1 border-b-1 animate__animated animate__fadeIn
       sm:-mt-0
       md:-mt-0">
-      <div>About me</div>
+      <h1 className="text-gray-700">About me</h1>
       <ul className="flex items-between justify-between gap-10">
         <li>Small profile picture w/ information</li>
         <li>Description Right?</li>
       </ul>
-      <div>skills
+      <div>
+        <h2>Skills</h2>
         <div className="flex ">
           <div>Front-end</div>
           <div>Back-end</div>
