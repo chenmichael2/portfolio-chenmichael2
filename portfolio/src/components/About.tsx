7451c1f184f19ba0922497a26a484cc4a28c1fcf
Michael Chen <chenmichael2012@gmail.com>
2025-08-03 18:01:02 -0700

    starting cards

--- portfolio/src/components/About.tsx
+++ portfolio/src/components/About.tsx
@@ -3,11 +3,11 @@ export default function About() {
     <section id="about" className="flex flex-col items-center justify-center w-full bg-gray-50 border-t-1 border-b-1 animate__animated animate__fadeIn
       sm:-mt-0
       md:-mt-0">
-      <h1 className="text-gray-700">About me</h1>
-      <ul className="flex items-between justify-between gap-10">
-        <li>Small profile picture w/ information</li>
-        <li>Description Right?</li>
-      </ul>
+      <h1 className="text-gray-700 text-2xl">About me</h1>
+      <div className="flex flex-col items-around justify-around gap-10">
+        <div className="border-1 border-red-800 rounded-md ">Small profile picture w/ information</div>
+        <div>Description Right?</div>
+      </div>
       <div>
         <h2>Skills</h2>
         <div className="flex ">
