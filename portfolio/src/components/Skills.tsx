c18b26a218f1935160f78fbb9810ceb8de421e85
Michael Chen <chenmichael2012@gmail.com>
2025-09-08 16:17:45 -0700

    adding skill name again

--- portfolio/src/components/Skills.tsx
+++ portfolio/src/components/Skills.tsx
@@ -23,7 +23,7 @@ export default function Skills() {
     {alt: "Django", icon: "/icons/technologies/Light/django.svg", name:"Django"},
     {alt: "MongoDB", icon: "/icons/technologies/Light/mongodb.svg", name:"MongoDB"},
     {alt: "TailwindCSS", icon: "/icons/technologies/Light/tailwindcss.svg", name:"Tailwind"},
-    {alt: "Node.js", icon: "/icons/technologies/Light/nodejs.svg", name:""},
+    {alt: "Node.js", icon: "/icons/technologies/Light/nodejs.svg", name:"Node.js"},
     {alt: "Github", icon: "/icons/technologies/Light/github.svg", name:""},
     {alt: "Hubspot", icon: "/icons/technologies/Light/hubspot.svg", name:""},
     {alt: "Crownpeak", icon: "/icons/technologies/Light/crownpeak.svg", name:""},
