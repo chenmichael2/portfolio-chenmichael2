09957695a4c70d32abe989008a32109f939f6950
Michael Chen <chenmichael2012@gmail.com>
2025-09-25 21:24:21 -0700

    adding experience id

--- portfolio/src/components/Experience.tsx
+++ portfolio/src/components/Experience.tsx
@@ -1,6 +1,6 @@
 export default function Experience() {
   return (
-    <section id="about" className="flex flex-col items-center justify-center w-full h-screen bg-gray-100 animate__animated animate__fadeIn -mt-0
+    <section id="experience" className="flex flex-col items-center justify-center w-full h-screen bg-gray-100 animate__animated animate__fadeIn -mt-0
       sm:-mt-0
       md:-mt-0">
       <div> This is the Experience</div>
