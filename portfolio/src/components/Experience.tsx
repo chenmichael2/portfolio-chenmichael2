54326e19939a532674256d034c24bf026377cb72
Michael Chen <chenmichael2012@gmail.com>
2025-10-31 09:21:36 -0700

    preping button

--- portfolio/src/components/Experience.tsx
+++ portfolio/src/components/Experience.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import gsap from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
 
@@ -73,6 +73,7 @@ export default function Experience() {
       }
     });
 
+    const [expButton, expButtonState] = useState("experience");
   }, []);
 
   return (
@@ -83,9 +84,9 @@ export default function Experience() {
         <h1 className="text-gray-700 text-4xl">Experience</h1>
         <div className="h-1 w-44 bg-gradient-to-r to-primary from-tertiary"></div>
       </div>
-      <div className="w-content border-1 flex flex-row justify-around p-1 gap-3 rounded-md bg-gradient-to-r to-primary from-tertiary">
+      <div id="expeButton" className="w-content border-1 flex flex-row justify-around p-1 gap-3 rounded-md bg-gradient-to-r to-primary from-tertiary">
         <button className="px-5 py-0.3 z-99 text-gray-50">Experience</button>
-        <div className="absolute w-30 h-6 -translate-x-15 rounded-sm bg-neutral-800/50 text-neutral-200 backdrop-blur-[2px] border-2 border-neutral-200/20 hover:bg-neutral-400/30"></div>
+        <div id="buttonSlider" className="absolute w-30 h-6 -translate-x-15 rounded-sm bg-neutral-800/50 text-neutral-200 backdrop-blur-[2px] border-2 border-neutral-200/20 hover:bg-neutral-400/30"></div>
         <button className="px-5 py-0.3 z-99 text-gray-50">Education</button>
       </div>
       <div id="exp">
