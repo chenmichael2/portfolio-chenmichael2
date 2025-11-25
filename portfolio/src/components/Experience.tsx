84d052027c386d437fffd21fb98ca73f7b3a7ae5
Michael Chen <chenmichael2012@gmail.com>
2025-11-24 18:25:21 -0800

    adding proper percentages

--- portfolio/src/components/Experience.tsx
+++ portfolio/src/components/Experience.tsx
@@ -14,9 +14,9 @@ export default function Experience() {
   }
 
   const dot: object = {
-    10: "blue", 
+    8: "blue", 
     25: "yellow", 
-    40: "red"
+    100: "red"
   };
 
   const cardInfo = {
@@ -110,12 +110,11 @@ export default function Experience() {
       </button>
       <div className={`pt-7 h-${timeHeight} w-full`}>
         <div id="timeline" className="flex justify-center">
-          <div className={`absolute z-40 w-2 h-${timeHeight} rounded-xl bg-gradient-to-b from-blue-500 via-purple-500 to-pink-500 transition-all ease-in-out duration-300`}
-          style={{ height: `${timeHeight}rem` }}></div>
+          <div className={`absolute z-40 w-2 rounded-xl bg-gradient-to-b from-blue-500 via-purple-500 to-pink-500 transition-all ease-in-out duration-300`}
+          style={{ height: `${timeHeight}rem` }} />
           {Object.entries(dot).map(([key, value]) => (
-            <Dot key={key} outside={value} inside="#f9fafb" percent={key}/>
+            <Dot key={key} outside={value} inside="#f9fafb" percent={String(Number(key) / timeHeight)}/>
           ))}
-          <Dot outside="blue" inside="#f9fafb" percent="20"/>
         </div>
         <div id="exp" className={`animate__animated w-auto ${expButton ? "inline animate__fadeInLeft" : "hidden animate__fadeOutLeft"} transition-all duration-300`}>
           <h2>This is the Experience</h2>
