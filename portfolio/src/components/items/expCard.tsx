7b941379d44a8a4809ec85c943633fa826adeec5
Michael Chen <chenmichael2012@gmail.com>
2025-12-14 17:28:10 -0800

    adding card details

--- portfolio/src/components/items/expCard.tsx
+++ portfolio/src/components/items/expCard.tsx
@@ -1,18 +1,18 @@
 
 interface ExpCardProp {
-    side?: string,
     title?: string, 
     dates?: string,
     jobTitle?: string, 
     description?: string, 
-
 }
-export default function ExpCard({side, title, dates}: ExpCardProp) {
+export default function ExpCard({title, dates, jobTitle, description}: ExpCardProp) {
 
     return (
         <div className="border-1 border-black rounded-sm p-2 px-4">
             <h2>{title}</h2>
             <p>{dates}</p>
+            <h3>{jobTitle}</h3>
+            <p>{description}</p>
         </div>
     );
 }
