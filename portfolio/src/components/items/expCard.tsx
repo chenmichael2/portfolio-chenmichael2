76446764b090ed7d29377788ca27c40504bb5f40
Michael Chen <chenmichael2012@gmail.com>
2025-12-15 17:04:18 -0800

    adding more

--- portfolio/src/components/items/expCard.tsx
+++ portfolio/src/components/items/expCard.tsx
@@ -8,7 +8,7 @@ interface ExpCardProp {
 export default function ExpCard({title, dates, jobTitle, description}: ExpCardProp) {
 
     return (
-        <div className="border-1 border-black rounded-sm p-2 px-4">
+        <div className="border-1 border-[#ccc] rounded-md shadow-xs p-2 px-4 bg-neutral-primary-soft">
             <h2>{title}</h2>
             <p>{dates}</p>
             <h3>{jobTitle}</h3>
