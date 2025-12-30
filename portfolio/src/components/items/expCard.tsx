6351a257b12d97b5a53232491c22d13d653b4a66
Michael Chen <chenmichael2012@gmail.com>
2025-12-29 22:07:57 -0800

    adding better timeline

--- portfolio/src/components/items/expCard.tsx
+++ portfolio/src/components/items/expCard.tsx
@@ -16,13 +16,11 @@ export default function ExpCard({ id, title, dates, jobTitle, description, top, 
         <div
             style={{ marginTop }}
             id={`${id}`}
-            className="exp-card self-start border-1 border-[#ccc] bg-white rounded-md shadow-sm p-2 pt-4 px-4 bg-neutral-primary-soft transition-all duration-300"
+            className="exp-card self-start border-1 border-[#ccc] bg-white rounded-md shadow-sm p-4 bg-neutral-primary-soft transition-all duration-300"
         >
-            <img src={`images/experience/${logo}`} alt={`${title} logo`} className="w-15 h-15 mb-2 shadow-lg rounded-md"/>
-            <h2 className="text-lg/5 font-medium">{title}</h2>
-            <p className="text-gray-700">{dates}</p>
-            <h3>{jobTitle}</h3>
-            <p>{description}</p>
+            <img src={`images/experience/${logo}`} alt={`${title} logo`} className="w-15 h-15 mb-4 shadow-lg rounded-md"/>
+            <h2 className="text-xl/6 font-medium">{title}</h2>
+            <h3 className="text-gray-700 leading-4.5">{jobTitle}</h3>
         </div>
     );
 }
