b680af510245ca57d887493f6d4293237b1f66ac
Michael Chen <chenmichael2012@gmail.com>
2025-12-30 20:23:13 -0800

    adding project page

---
+++ portfolio/src/components/Project.tsx
@@ -0,0 +1,56 @@
+'use client';
+import { gsap } from "gsap"
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useEffect } from "react";
+
+
+
+export default function Project() {
+  
+  useEffect(() => {
+
+    ScrollTrigger.matchMedia({
+      "(min-width: 768px)": () => { // For smaller screens, apply the initial animation
+        gsap.fromTo(".hero-section", 
+          {
+            y: 0, 
+            opacity: 1 
+          }, {
+          scrollTrigger: {
+            trigger: ".hero-section",
+            start: "30% 20%",
+            end: "bottom 20%",
+            scrub: 1, 
+            toggleActions: "play none none reverse",
+          },
+          y: -50,
+          opacity: 0,
+        });
+      },
+      "(max-width: 769px)": () => { // For larger screens, apply a different animation
+        gsap.fromTo(".hero-section", 
+          {
+            y: 0, 
+            opacity: 1
+          }, {
+          scrollTrigger: {
+            trigger: ".hero-section",
+            start: "50% 25%",
+            end: "bottom 25%",
+            scrub: 0.5,
+            toggleActions: "play none none reverse",
+          },
+          y: -10,
+          opacity: 0,
+        });
+      }
+    });
+
+  }, []);
+
+  return (
+    <section className="hero-section w-full min-h-240 h-[calc(100vh-1rem)] max-h-300 bg-transparent pt-15">
+
+    </section>
+  );
+}
