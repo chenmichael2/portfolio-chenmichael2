11f5622dde1ea12a8d7a21601514fde3acc69e7e
Michael Chen <chenmichael2012@gmail.com>
2026-01-23 12:55:20 -0800

    adding flex

--- portfolio/src/components/items/projCard.tsx
+++ portfolio/src/components/items/projCard.tsx
@@ -8,14 +8,18 @@ interface ProjCardProp {
 export default function ProjCard({ image, title, description, links}: ProjCardProp) {
 
     return (
-        <div className="border-1 border-[#ccc] w-[100%] p-4 rounded-xl shadow-xl">
-            <img src={`/images/projects/` + image} className="rounded-lg"/>
-            <div className="mt-4">
-                <h2 className="text-xl">{title}</h2>
-                <p>{description}</p>
+        <div className="border-1 border-[#ccc] w-[100%] p-4 h-100 rounded-xl shadow-xl flex flex-col justify-between">
+            <div>
+                <img src={`/images/projects/` + image} className="rounded-lg"/>
+                <div className="mt-4">
+                    <h2 className="text-xl">{title}</h2>
+                    <p>{description}</p>
+                </div>
+            </div>
+            <div>
                 {links && Object.entries(links).map(([key, value], index) => {
                     return(
-                        <p key={index}>{key}:{value}</p>
+                        <div key={index}>{key}:{value}</div>
                     )
                 })}
             </div>
