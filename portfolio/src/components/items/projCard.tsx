c49af7d44994b6f0e83e47c93b1169c2b2a1ccf6
Michael Chen <chenmichael2012@gmail.com>
2026-01-22 18:16:55 -0800

    adding text

--- portfolio/src/components/items/projCard.tsx
+++ portfolio/src/components/items/projCard.tsx
@@ -11,7 +11,7 @@ export default function ProjCard({ image, title, description, links}: ProjCardPr
         <div className="border-1 border-[#ccc] w-[100%] p-4 rounded-xl shadow-xl">
             <img src={`/images/projects/` + image} className="rounded-lg"/>
             <div className="mt-4">
-                <h2 className="text-lg">{title}</h2>
+                <h2 className="text-xl">{title}</h2>
                 <p>{description}</p>
                 {links && Object.entries(links).map(([key, value], index) => {
                     return(
