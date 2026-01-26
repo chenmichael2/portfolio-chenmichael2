582af9edd5e7078abb59c950cd46ebfc2ec3da5b
Michael Chen <chenmichael2012@gmail.com>
2026-01-25 17:39:49 -0800

    adjusting img

--- portfolio/src/components/items/projCard.tsx
+++ portfolio/src/components/items/projCard.tsx
@@ -13,7 +13,7 @@ export default function ProjCard({ image, title, description, links}: ProjCardPr
     return (
         <div className="border-1 border-[#ccc] w-[100%] p-4 h-100 rounded-xl shadow-xl flex flex-col justify-between">
             <div>
-                <img src={`/images/projects/` + image} className="rounded-lg"/>
+                <img src={`/images/projects/` + image} className="rounded-lg w-[100%]"/>
                 <div className="mt-4">
                     <h2 className="text-xl">{title}</h2>
                     <p>{description}</p>
@@ -32,8 +32,8 @@ export default function ProjCard({ image, title, description, links}: ProjCardPr
                         
                     }
                         return(
-                            <button key={index}>
-                                {key}:{value}
+                            <button key={index} className='w-[20px] '>
+                                {renderContent()}
                             </button>
                         )
                 })}
