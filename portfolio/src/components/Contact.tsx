13cad8e44dbc404d63fbd16a261f967443dfbe53
Michael Chen <chenmichael2012@gmail.com>
2026-02-09 17:33:21 -0800

    adding space

--- portfolio/src/components/Contact.tsx
+++ portfolio/src/components/Contact.tsx
@@ -2,7 +2,9 @@ export default function Contact() {
     return (
         <section className="">
             <div>
-                <div></div>
+                <div>
+                    
+                </div>
             </div>
         </section>
     )
